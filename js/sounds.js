a1e48feea5b7a4550894da7f3ccd600e210a1216
jannes lambrecht <janneslambrecht@hotmail.com>
2024-08-19 23:44:29 +0200

    update

---
+++ js/sounds.js
@@ -0,0 +1,28 @@
+const soundboardElement = document.getElementById('soundboard');
+const toggleDarkModeCheckbox = document.getElementById('toggleDarkMode');
+const page = document.body;
+
+// Load sounds and backgrounds from a JSON file
+fetch('./js/sounds.json')
+    .then(response => response.json())
+    .then(data => {
+        data.sounds.forEach(sound => {
+            const button = document.createElement('button');
+            button.className = 'soundboard__button';
+            button.style.backgroundImage = `url(${sound.background})` || '../assets/images/fallback_background.jpg';
+            button.textContent = sound.label;
+
+            button.addEventListener('click', () => {
+              const audio = new Audio(sound.url || 'fallback_sound.mp3');
+              audio.play();
+            });
+
+            soundboardElement.appendChild(button);
+        });
+    })
+    .catch(error => console.error('Error loading sounds:', error));
+
+// Toggle dark mode
+toggleDarkModeCheckbox.addEventListener('change', () => {
+    page.classList.toggle('page--dark', toggleDarkModeCheckbox.checked);
+});
